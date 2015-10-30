0c1c8501b620eeb1661b80b8a546ecf9b7e3435c
Hlynur Halldórsson <hlynur@i-7f3b5dd9.csd07cloud.internal>
2015-10-30 20:03:25 +0000

    Fixes for rendering html file

---
+++ src/main/java/com/tictactoe/main/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.tictactoe.main;	
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("ATLIHOMMI");
+    }
+}
---
+++ src/test/java/com/tictactoe/main/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.tictactoe.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest{
+
+    @Test
+    public void greetResultsInHello() {
+        assertSame(1, 1); }
+
+}
