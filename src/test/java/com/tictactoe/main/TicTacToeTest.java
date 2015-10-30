d176e2d9bd57295dd858be1819427cae581b7917
Helgi Óskarsson <helgi@i-7f3b5dd9.csd07cloud.internal>
2015-10-30 17:33:32 +0000

    added board test

--- src/test/java/com/tictactoe/main/TicTacToeTest.java
+++ src/test/java/com/tictactoe/main/TicTacToeTest.java
@@ -9,5 +9,10 @@ public class TicTacToe{
     @Test
     public void greetResultsInHello() {
         assertSame(1, 1); }
+    @Test
+	//Add x to board, field 2,3
+    public void newBoard() {
+        assertSame(x, 2, 3, true); }
+
 
 }
