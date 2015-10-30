deb05da978788edbeb37034e906f54e82e94e704
brynjas <brynja13@ru.is>
2015-10-30 22:53:28 +0000

    conliftg

---
+++ src/main/java/com/tictactoe/main/Player.java
@@ -0,0 +1,15 @@
+package src.main.java.com;
+
+public class Player{
+
+    private char PlayerSymbol;
+   
+    public Player (char s){
+	PlayerSymbol = s;
+    }
+    
+    public char getPlayerSymbol(){
+	return PlayerSymbol;
+    }
+   
+}
