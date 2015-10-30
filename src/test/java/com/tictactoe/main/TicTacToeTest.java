1e0280a8ab89311993b4c4b79b3b4599c4091e34
Helgi Óskarsson <helgi@i-7f3b5dd9.csd07cloud.internal>
2015-10-30 20:12:38 +0000

    getMark unit test, other test fixed

--- src/test/java/com/tictactoe/main/TicTacToeTest.java
+++ src/test/java/com/tictactoe/main/TicTacToeTest.java
@@ -12,17 +12,40 @@ public class TicTacToeTest{
     @Test
 	//Test if Board class creates 3*3 char array
     public void newBoard() {
-	char[][] testArray = new char[3][3];
+	char[][] testArray = new char[3][3];      
+        for(int i = 0; i < 3; i++){
+        	for(int j = 0; j < 3; j++){
+                	testArray[i][j] = '-';
+                }
+        }
+
 	Board testBoard = new Board();
         assertArrayEquals(testArray, testBoard.getBoard());
     }
     @Test
     public void markBoard() {
 	char[][] testArray = new char[3][3];
+	for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                        testArray[i][j] = '-';
+                }
+        }
 	testArray[1][2] = 'x';
 	Board testBoard = new Board();
 	testBoard.mark(1, 2, 'x');
 	assertArrayEquals(testArray, testBoard.getBoard());
+    }
+    @Test
+    public void getMarkTest() {
+        Board testBoard = new Board();
+        testBoard.mark(1, 2, 'x');
+        assertSame('x', testBoard.getMark(1, 2));
+    }
+
+    @Test
+    public void getMarkTestEmpty() {
+        Board testBoard = new Board();
+        assertSame('-', testBoard.getMark(0, 1));
     }
 
 
