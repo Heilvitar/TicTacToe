b96851993fcea30e556c4e27d60604a66b3ac878
Ragna lLára Ellertsdóttir <ragna@i-7f3b5dd9.csd07cloud.internal>
2015-10-31 00:51:35 +0000

    tests for players and board

--- src/test/java/com/tictactoe/main/GameTest.java
+++ src/test/java/com/tictactoe/main/GameTest.java
@@ -4,12 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class GameTest()
+public class GameTest
 {
-	//@Te 
+	Game testGame = new Game();
 
-
-
-
-
+        @Test
+        public void testPlayer1Test(){
+		assertSame('x', testGame.player1.getPlayerSymbol());
+        }
+        @Test
+        public void testPlayer2Test(){
+                assertSame('o', testGame.player2.getPlayerSymbol());
+        }	
+ 	public void testBoard(){
+                assertSame('-', testGame.gameBoard.getBoard()[0][0]);
+        }
 }
