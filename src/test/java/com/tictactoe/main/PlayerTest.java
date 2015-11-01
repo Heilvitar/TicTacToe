9ba8109d4b769a894b781bce3d28eab0b5c1b4af
brynjas <brynja13@ru.is>
2015-11-01 20:25:00 +0000

    unit test for player class

---
+++ src/test/java/com/tictactoe/main/PlayerTest.java
@@ -0,0 +1,20 @@
+package com.tictactoe.main;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PlayerTest {
+
+    final Player pX = new Player('X');
+    final Player pO = new Player('O');
+
+    @Test
+    public void testPlayerSymbol_X() {
+        assertEquals('X', pX.getSymbol());
+    }
+    @Test
+    public void testPlayerSymbol_O() {
+        assertEquals('O', pO.getSymbol());
+    }
+
+}
